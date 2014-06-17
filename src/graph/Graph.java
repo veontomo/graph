8b06d921f8dc712122273056778749c62cfb29dd
Andrea Shcherbakov <a.shcherbakov@ritoll.it>
2014-06-17 16:27:55 +0200

    Class and tests are started

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -15,11 +15,17 @@ import java.util.*;
 public class Graph {
     
     /**
-     * A map from node number to array that must contain two
-     * elements only: the first one is a list of out-nodes, the 
-     * second - is a list of in-nodes for the given node.
+     * A map from node number to list of node numbers corresponding to nodes 
+     * from which it is possibile to arrive to the given node.
      */
-    private HashMap<Integer, List<Integer>[]> _edges;
+    private HashMap<Integer, Set<Integer>> _edgesIn;
+    
+    /**
+     * A map from node number to list of node numbers corresponding to nodes
+     * to which it is possibile to arrive from the given node.
+     */
+    private HashMap<Integer, Set<Integer>> _edgesOut;
+
     
     /**
      * A map from node number to finishing time. For nodes that have already been 
@@ -29,25 +35,66 @@ public class Graph {
     private HashMap<Integer, Integer> _time;
     
     /**
-     * A map from node number to list of node numbers for which the key
-     * node is a leader.
+     * A map from node number to leader node number.
      */
-    private HashMap<Integer, List[]> _leader;
+    private HashMap<Integer, Integer> _leader;
     
+    /**
+     * Graph constructor. 
+     */
+    public Graph(){
+        this._edgesIn = new HashMap();
+        this._edgesOut = new HashMap();
+        this._time = new HashMap();
+        this._leader = new HashMap();
+    }
 
     /**
+     * Registers edge inside src.
+     * If src has no key i, adds it. Then, if the set corresponding to key value i 
+     * does not contain value j, then insert j into this set and returns true. 
+     * If the corresponding set contains value j, false is returned.
+     * @param src  where to register the edge: either _edgesIn or _edgesOut.
+     * @param i node number
+     * @param j node number
+     * @return  boolean
+     */
+    private boolean _registerEdge(HashMap<Integer, Set<Integer>> src, Integer i, Integer j){
+        if (src.containsKey(i)){
+            if(src.get(i).contains(j)){
+                return false;
+            };
+            src.get(i).add(j);
+        } else {
+            Set<Integer> list = new HashSet();
+            list.add(j);
+            src.put(i, list);
+        }
+        return true;
+    };
+    
+    /**
      * Adds directed edge going out from node number tail and entering node head.
-     * @param tail
-     * @param head 
+     * @param tail number of tail node
+     * @param head number of head node
      */
     public void addEdge(Integer tail, Integer head){
-        if (_edges.get(tail) == null){
-            Integer[][] currentNodeEdges;
-            currentNodeEdges = new Integer[2][2];
-            
-        } else {
-        
+        this._registerEdge(this._edgesOut, tail, head);
+        this._registerEdge(this._edgesIn, head, tail);
+    }
+    
+    /**
+     * Returns true if there exist an edge from node tail to node head and false
+     * otherwise.
+     * @param tail number of tail node
+     * @param head number of head node
+     * @return 
+     */
+    public boolean edgeExists(Integer tail, Integer head){
+        if (!this._edgesOut.containsKey(tail)){
+            return false;
         }
+        return this._edgesOut.get(tail).contains(head);
     }
             
     
@@ -56,6 +103,7 @@ public class Graph {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("start graph");
     }
     
 }
--- test/graph/GraphTest.java
+++ test/graph/GraphTest.java
@@ -6,9 +6,7 @@
 
 package graph;
 
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -29,25 +27,89 @@ public class GraphTest {
     @AfterClass
     public static void tearDownClass() {
     }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+
+    /**
+     * Test of edgeExists method, of class Graph.
+     */
+    @Test
+    public void testEdgeExists() {
+        System.out.println("Test whether just added edge exists");
+        Graph g = new Graph();
+        g.addEdge(5, 7);
+        boolean result = g.edgeExists(5, 7);
+        assertEquals(true, result);
     }
 
+    /**
+     * Test of edgeExists method, of class Graph.
+     */
+    @Test
+    public void testEdgeExists2() {
+        System.out.println("Test whether missing edge exists");
+        Graph g = new Graph();
+        g.addEdge(1, 2);
+        boolean result = g.edgeExists(5, 7);
+        assertEquals(false, result);
+    }
+
+    /**
+     * Test of edgeExists method, of class Graph.
+     */
+    @Test
+    public void testEdgeExistsIfInverse() {
+        System.out.println("Must return false if the the requested edge does not exists, but the opposite does exist.");
+        Graph g = new Graph();
+        g.addEdge(3, 2);
+        boolean result = g.edgeExists(2, 3);
+        assertEquals(false, result);
+    }
+
+    /**
+     * Test of edgeExists method, of class Graph.
+     */
+    @Test
+    public void testEdgeExistsIfSelfLoopExists() {
+        System.out.println("Must return true if asked about a self-loop that exists.");
+        Graph g = new Graph();
+        g.addEdge(10, 10);
+        boolean result = g.edgeExists(10, 10);
+        assertEquals(true, result);
+    }
+
+    /**
+     * Test of edgeExists method, of class Graph.
+     */
+    @Test
+    public void testEdgeExistsIfSelfLoopNotExists() {
+        System.out.println("Must return false if asked about a self-loop that does not exist.");
+        Graph g = new Graph();
+        g.addEdge(3, 3);
+        boolean result = g.edgeExists(1, 1);
+        assertEquals(false, result);
+    }
+
+    
+    /**
+     * Test of edgeExists method, of class Graph.
+     */
+    @Test
+    public void testEdgeExistsOfEmptyGraph() {
+        System.out.println("Test whether an edgge exists in empty graph");
+        Graph g = new Graph();
+//        boolean result = g.edgeExists(1, 3);
+//        assertEquals(false, result);
+    }
+    
     /**
      * Test of main method, of class Graph.
      */
     @Test
     public void testMain() {
-        System.out.println("main");
-        String[] args = null;
-        Graph.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+//        System.out.println("main");
+//        String[] args = null;
+//        Graph.main(args);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
     }
     
 }
